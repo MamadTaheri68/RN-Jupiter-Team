4bef8a4e612db8a1334fe469c2560b1aea5081b1
MamadTaheri68 <mamad.taheri.68@gmail.com>
2022-05-06 16:16:15 +0430

    add dev-team and staff section

---
+++ dev-team/01-mohammad-taheri.js
@@ -0,0 +1,14 @@
+const mohammadTaheri = {
+  id: 1822,
+  fullName: "Mohammad Taheri",
+  email: "mamad.taheri.68@gmail.com",
+  linkedin: "https://www.linkedin.com/in/mohammad-taheri1/",
+  github: "https://github.com/MamadTaheri68",
+  jobTitle: "Frontend Developer at N.A.P (React)",
+  About: `I'm Mohammad. I am a software engineer from Iran. 
+        I help companies to build web applications using react, JavaScript, Typescript along with modern related libraries and technologies.
+        I have experience in working with:
+        *HTML/ CSS/ JAVASCRIPT/ TYPESCRIPT/ REACT`,
+};
+
+module.exports = mohammadTaheri;
---
+++ dev-team/Staff.js
@@ -0,0 +1,7 @@
+const mohammadTaheri = require("./01-mohammad-taheri");
+
+const Staff = [
+    mohammadTaheri,
+];
+
+module.exports = Staff;
