6291cd1fd31c2e9da49661c51411ee15cd12e55a
emel95 <amalmosavi95@gmail.com>
2022-05-09 01:04:38 +0430

    about EmelMousavi

---
+++ dev-team/02-EmelMousavi.js
@@ -0,0 +1,13 @@
+const EmelMousavi = {
+  id: 1374,
+  fullName: "Emel Mousavi",
+  email: "amalmosavi95@gmail.com",
+  linkedin: "https://www.linkedin.com/in/emel-mosavi-778b7b221/",
+  github: "https://github.com/emel95",
+  jobTitle: "Frontend Developer React Native",
+  About: `I'm EmelMousavi. I am a software engineer from Iran. 
+  I'm very interested in programming and I enjoy coding,
+   I do not have much experience but instead I have high hopes for my future`,
+};
+
+module.exports = EmelMousavi;
