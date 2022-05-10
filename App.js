b5bfe17316c81aeb71388f1f797adbbc99b3cc6f
emel95 <amalmosavi95@gmail.com>
2022-05-10 14:09:30 +0430

    add Skeleton to product Component

--- App.js
+++ App.js
@@ -9,99 +9,20 @@ import {
   View,
 } from 'react-native';
 
-import Product from './src/components/Products';
-
-import { Colors } from 'react-native/Libraries/NewAppScreen';
-
-const tempProducts = new Array(40).fill(0);
-
-const Section = ({ children, title }) => {
-  const isDarkMode = useColorScheme() === 'dark';
-  return (
-    <View style={styles.sectionContainer}>
-      <Text
-        style={[
-          styles.sectionTitle,
-          {
-            color: isDarkMode ? Colors.white : Colors.black,
-          },
-        ]}>
-        {title}
-      </Text>
-      <Text
-        style={[
-          styles.sectionDescription,
-          {
-            color: isDarkMode ? Colors.light : Colors.dark,
-          },
-        ]}>
-        {children}
-      </Text>
-    </View>
-  );
-};
+import Product from './src/components/Product';
+import ProductList from './src/components/ProductList';
 
 const App = () => {
-  const isDarkMode = useColorScheme() === 'dark';
 
-  const backgroundStyle = {
-    backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
-  };
 
   return (
-    // <View style={{ flex: 1 }}>
-    //   <Product />
-    // </View>
+    <View style={{ flex: 1 }}>
+      <Product />
+      {/* <ProductList/> */}
+    </View>
 
-    <SafeAreaView style={backgroundStyle}>
-      <StatusBar barStyle={isDarkMode ? 'light-content' : 'dark-content'} />
-     
-      <ScrollView
-        contentInsetAdjustmentBehavior="automatic"
-        style={backgroundStyle}>
-        <View
-          style={{
-            backgroundColor: isDarkMode ? Colors.black : Colors.white,
-          }}>
-          {tempProducts.map((q, i) => (
-            <View style={styles.card}>
-              <Section title={`Product #${i + 1}`}>
-                Lorem ipsum {Math.floor(Math.random() * 100000)}
-              </Section>
-            </View>
-          ))}
-        </View>
-      </ScrollView>
-    </SafeAreaView>
+    
   );
 };
-
-const styles = StyleSheet.create({
-  sectionContainer: {
-    marginTop: 5,
-    paddingHorizontal: 24,
-  },
-  sectionTitle: {
-    fontSize: 24,
-    fontWeight: '600',
-  },
-  sectionDescription: {
-    marginTop: 8,
-    fontSize: 18,
-    fontWeight: '400',
-  },
-  highlight: {
-    fontWeight: '700',
-  },
-  card: {
-    padding: 0,
-    borderColor: '#E5E5E5',
-    borderStyle: "solid",
-    borderWidth: 2,
-    borderRadius: 8,
-    margin: 5,
-
-  }
-});
 
 export default App;
