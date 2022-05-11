b43bbf7c31f97d4c8aea6402728e50b51980d5fd
emel95 <amalmosavi95@gmail.com>
2022-05-11 11:13:52 +0430

    create componentProductList

--- src/components/Product.js
+++ src/components/Product.js
@@ -9,11 +9,15 @@ import SkeletonPlaceholder from "react-native-skeleton-placeholder";
 
 const { width, height } = Dimensions.get('screen');
 
-const Product = () => {
+const Product = ({ valueProduct, isLoading }) => {
 
-  const [loadding, setLoading] = useState(true);
-  let Description = 'Leather Key Ring - Wisteria ghdgfdgfd fbggfdgf fggfd dffhd ';
-  let Deal = 25.53;
+  const { title, image, brand, price, review } = valueProduct;
+  const { main, deal } = price;
+  const { people, stars } = review;
+
+  // const [loadding, setLoading] = useState(false);
+  // let Description = 'Leather Key Ring - Wisteria ghdgfdgfd fbggfdgf fggfd dffhd ';
+  // let Deal = 25.53;
 
 
   const ratingCompleted = (rating) => {
@@ -23,7 +27,7 @@ const Product = () => {
   return (
     <View style={{ flex: 1, backgroundColor: '#fff' }}>
 
-      {loadding === true ?
+      {isLoading.isLoading === true ?
         (<View style={styles.cardProduct}>
           <SkeletonPlaceholder highlightColor='grey' speed={2900}>
             < View style={styles.skeletonRowTop} />
@@ -44,20 +48,20 @@ const Product = () => {
             <View style={styles.rowTop}>
 
               {/* <AirbnbRating
-          count={5}
-          //  reviews={["Bad", "OK", "Good", "Very Good", "Wow"]}
-          defaultRating={3}
-          size={15}
-          selectedColor='#f1c40f'
-          unSelectedColor='#eee8aa'
-          // reviewSize={25}
-          // reviewColor='red'
-          showRating={false}
+                count={5}
+                //  reviews={["Bad", "OK", "Good", "Very Good", "Wow"]}
+                defaultRating={3}
+                size={15}
+                selectedColor='#f1c40f'
+                unSelectedColor='#eee8aa'
+                // reviewSize={25}
+                // reviewColor='red'
+                showRating={false}
 
-        /> */}
+              /> */}
               <Rating
                 type='custom'
-                startingValue={4.5}
+                startingValue={stars}
                 ratingCount={5}
                 ratingColor='#f1c40f'
                 ratingBackgroundColor='#fff'
@@ -68,26 +72,30 @@ const Product = () => {
                 jumpValue={0.5}
                 onSwipeRating={(value) => ratingCompleted(value)}
               />
-              <Text style={styles.txtScores}>6979</Text>
+              <Text style={styles.txtScores}>{people}</Text>
             </View>
             <View style={styles.rowBottom}>
               <View style={styles.columnLeft}>
 
                 <View style={styles.viewTitleDescription}>
 
-                  <Text style={styles.txtTitle}>Sponsored Ad - Apple AirTag</Text>
-                  {Description && <Text style={styles.txtDescription}>
-                    {Description.length <= 27 ? Description : `${Description.substring(0, 27)}...`}
+                  <Text style={styles.txtTitle}>{title}</Text>
+                  {brand && <Text style={styles.txtDescription}>
+                    {brand.length <= 27 ? brand : `${brand.substring(0, 27)}...`}
                   </Text>}
 
                 </View>
 
                 <View style={styles.viewPriceDeal}>
-                  {Deal && <Text style={styles.txtPriceDeal}>${Deal} </Text>}
-                  <Text style={styles.txtDeal}>تخفیف</Text>
+                  {deal && (<>
+                    <Text style={styles.txtPriceDeal}>${deal} </Text>
+                    <Text style={styles.txtDeal}>تخفیف</Text>
+                  </>
+
+                  )}
                 </View>
 
-                <Text style={styles.txtPriceMain}>$876</Text>
+                <Text style={styles.txtPriceMain}>${main}</Text>
 
               </View>
 
@@ -99,7 +107,7 @@ const Product = () => {
             </View>
           </View>
         )}
-      <View style={styles.separator} />
+
     </View>
 
   )
--- src/components/ProductList.js
+++ src/components/ProductList.js
@@ -1,116 +1,104 @@
 
-import React, { useState ,useEffect} from 'react';
-import {
-    StyleSheet,
-    Text,
-    FlatList,
-    View,
-    Dimensions,
-    ActivityIndicator,
-} from 'react-native';
-
+import React, { useState, useEffect } from 'react';
+import { StyleSheet, Text, FlatList, View, Dimensions, ActivityIndicator, Alert } from 'react-native';
+import Product from './Product';
+import { styles } from '../assets/styles/productStyle';
 const { width, height } = Dimensions.get('screen');
 
 
-const ProductList = () => {
-    const [loading, setLoading] = useState(false);
-    const [dataSource, setDataSource] = useState([]);
-    const [offset, setOffset] = useState(1);
-    const [isListEnd, setIsListEnd] = useState(false);
-    useEffect(() => getData(), []);
+const ProductList = ({ valueProducts, isLoading }) => {
+    console.log('valueProducts', valueProducts);
+    console.log('isLoading', isLoading);
+    // const [loading, setLoading] = useState(false);
+    // const [dataSource, setDataSource] = useState([]);
+    // const [offset, setOffset] = useState(1);
+    // const [isListEnd, setIsListEnd] = useState(false);
+    // useEffect(() => getData(), []);
 
-    const getData = () => {
-        console.log(offset);
-        if (!loading && !isListEnd) {
-            console.log('getData');
-            setLoading(true);
-            // Service to get the data from the server to render
-            fetch('https://aboutreact.herokuapp.com/getpost.php?offset='
-                + offset)
-                // Sending the currect offset with get request
-                .then((response) => response.json())
-                .then((responseJson) => {
-                    // Successful response from the API Call
-                    console.log(responseJson);
-                    if (responseJson.results.length > 0) {
-                        setOffset(offset + 1);
-                        // After the response increasing the offset
-                        setDataSource([...dataSource, ...responseJson.results]);
-                        setLoading(false);
-                    } else {
-                        setIsListEnd(true);
-                        setLoading(false);
-                    }
-                })
-                .catch((error) => {
-                    console.error(error);
-                });
-        }
-    };
+    // const getData = () => {
+    //     console.log(offset);
+    //     if (!loading && !isListEnd) {
+    //         console.log('getData');
+    //         setLoading(true);
+    //         // Service to get the data from the server to render
+    //         fetch('https://aboutreact.herokuapp.com/getpost.php?offset='
+    //             + offset)
+    //             // Sending the currect offset with get request
+    //             .then((response) => response.json())
+    //             .then((responseJson) => {
+    //                 // Successful response from the API Call
+    //                 console.log(responseJson);
+    //                 if (responseJson.results.length > 0) {
+    //                     setOffset(offset + 1);
+    //                     // After the response increasing the offset
+    //                     setDataSource([...dataSource, ...responseJson.results]);
+    //                     setLoading(false);
+    //                 } else {
+    //                     setIsListEnd(true);
+    //                     setLoading(false);
+    //                 }
+    //             })
+    //             .catch((error) => {
+    //                 console.error(error);
+    //             });
+    //     }
+    // };
 
     const renderFooter = () => {
         return (
             // Footer View with Loader
-            <View style={styles.footer}>
-                {loading ? (
+            <View style={{
+                padding: 10,
+                justifyContent: 'center',
+                alignItems: 'center',
+                flexDirection: 'row'
+            }}>
+                {isLoading.isLoading ? (
                     <ActivityIndicator
-                        color="black"
+                        color="grey"
                         style={{ margin: 15 }} />
                 ) : null}
             </View>
         );
     };
 
-    const ItemView = ({ item }) => {
-        return (
-            // Flat List Item
-            <Text
-                style={styles.itemStyle}
-                onPress={() => getItem(item)}>
-                {item.id}
-                {'.'}
-                {item.title.toUpperCase()}
-            </Text>
-        );
-    };
+    // const ItemView = ({ item }) => {
+    //     return (
+    //         // Flat List Item
+    //         <Text
+    //             style={styles.itemStyle}
+    //             onPress={() => getItem(item)}>
+    //             {item.id}
+    //             {'.'}
+    //             {item.title.toUpperCase()}
+    //         </Text>
+    //     );
+    // };
 
     const ItemSeparatorView = () => {
         return (
             // Flat List Item Separator
-            <View
-                style={{
-                    height: 0.5,
-                    width: '100%',
-                    backgroundColor: '#C8C8C8',
-                }}
-            />
+            <View style={styles.separator} />
         );
     };
 
-    const getItem = (item) => {
-        // Function for click on an item
-        alert('Id : ' + item.id + ' Title : ' + item.title);
-    };
+    // const getItem = (item) => {
+    //     // Function for click on an item
+    //     alert('Id : ' + item.id + ' Title : ' + item.title);
+    // };
     return (
         <View style={{ flex: 1 }}>
             <FlatList
-                data={dataSource}
-                keyExtractor={(item, index) => index.toString()}
+                data={valueProducts}
+                keyExtractor={(product) => product.id}
                 ItemSeparatorComponent={ItemSeparatorView}
-                renderItem={ItemView}
+                renderItem={({ item }) => <Product valueProduct={item} isLoading={isLoading} />}
                 ListFooterComponent={renderFooter}
-                onEndReached={getData}
+                //  onEndReached={({item})=>getData(item)}
                 onEndReachedThreshold={0.5}
             />
         </View>
     )
 }
-const styles = StyleSheet.create({
-    footer: {
-        padding: 10,
-        justifyContent: 'center',
-        alignItems: 'center',
-        flexDirection: 'row',
-    },
-});
+
 export default ProductList;
