c70d18827f9bc70a443a545279fec18d3b68ef21
emel95 <amalmosavi95@gmail.com>
2022-05-19 16:48:54 +0430

    handel pagination with requst api and redux

--- src/redux/action/productAction.js
+++ src/redux/action/productAction.js
@@ -1,26 +1,47 @@
-// import SearchProductsByTitleService from '../../services/SearchProductsByTitleService'
+import SearchProductsByTitleService from '../../services/SearchProductsByTitleService'
+import PageingProductsByTitleService from './../../services/PageingProductsByTitleService';
 
-export const productAction = (params) => {
+
+export const productsAction = (queryString) => {
     return async (dispatch) => {
         try {
-            await dispatch({ type: 'API_REQUEST', payload: { page: params.page } });
-            // const productState = await SearchProductsByTitleService();
-            // call api request
-            productState=[
-                {id:1,title:'jnkjnjk'},
-                {id:2,title:'jnkjnjk'},
-                {id:3,title:'jnkjnjk'},
-                {id:4,title:'jnkjnjk'},
-                {id:5,title:'jnkjnjk'},
-                {id:6,title:'jnkjnjk'},
-                {id:7,title:'jnkjnjk'},
-                {id:8,title:'jnkjnjk'}
-        ]
-            if (productState.length > 0) {
-                await dispatch({ type: 'API_SUCCESS', data: productState });
+            await dispatch({ type: 'API_REQUEST', payload: { isPaging: false } });
+            const serviceResult = await SearchProductsByTitleService(queryString);
+            const { pages, products } = serviceResult;
+            console.log('pagesactionOnepagebefromserver',pages);
+            if (products.length > 0) {
+                await dispatch({ type: 'API_SUCCESS_PAGE_ONE', payload: { products:products, pages:pages } });
             } else {
                 await dispatch({ type: 'API_LIST_END' })
             }
+        } catch (err) {
+            console.log(err);
+            await dispatch({ type: 'API_FAILURE', error: err.message });
+
+        }
+
+    }
+}
+export const productMoreActions = (queryString) => {
+    return async (dispatch, getState) => {
+        try { 
+            const {pages} = { ...getState().producReducer };
+            await dispatch({ type: 'API_REQUEST', payload: { isPaging: true } });
+           
+            console.log('value',pages)
+            const indexPage = pages.findIndex(item => item.check === true);
+            pages[indexPage].check = false;
+            indexNextPage=indexPage + 1;
+            pages[indexNextPage].check = true;
+            let pageNext = pages.find(item => item.check === true);
+            if (pageNext.link !== null) {
+                const products = await PageingProductsByTitleService(queryString,pageNext.link);
+                 await dispatch({ type: 'API_SUCCESS_PAGE_OTHER', payload: { products } });   
+                
+            } else {
+                await dispatch({ type: 'API_LIST_END' })
+            }
+
         } catch (err) {
             console.log(err);
             await dispatch({ type: 'API_FAILURE', error: err.message });
--- src/redux/reducer/producReducer.js
+++ src/redux/reducer/producReducer.js
@@ -2,24 +2,37 @@ const initialState = {
     loading: false,
     moreLoading: false,
     error: null,
-    // moreError: null,
     data: [],
+    pages: [],
     isListEnd: false,
 }
 export const producReducer = (state = initialState, action) => {
     console.log({ action });
     switch (action.type) {
         case 'API_REQUEST':
-            if (action.payload.page === 1) {
+            if (!action.payload.isPaging) {
                 return { ...state, loading: true }
             } else {
                 return { ...state, moreLoading: true }
             }
 
-        case 'API_SUCCESS':
+
+        case 'API_SUCCESS_PAGE_ONE':
             return {
                 ...state,
-                data: [...state.data, ...action.data],
+                data: [...action.payload.products],
+                pages: [...action.payload.pages],
+                error: '',
+                loading: false,
+                moreLoading: false,
+
+            }
+
+        case 'API_SUCCESS_PAGE_OTHER':
+            console.log('pagereducersave page in state', state.pages);
+            return {
+                ...state,
+                data: [...state.data, ...action.payload.products],
                 error: '',
                 loading: false,
                 moreLoading: false
@@ -32,7 +45,6 @@ export const producReducer = (state = initialState, action) => {
                 loading: false,
                 moreLoading: false
             }
-
         case 'API_LIST_END':
             return {
                 ...state,
@@ -41,6 +53,7 @@ export const producReducer = (state = initialState, action) => {
                 moreLoading: false
             }
 
-        default: return state;
+        default:
+            return state;
     }
 }
